b10cbe165ad2ba76cf54ce8c082589db3009da0d
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-09-30 16:59:54 -0400

    total con descuento e impuesto para uno solo

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -26,6 +26,24 @@ describe("Totalizador ", () => {
   it("deberia calcular para un descuento", () => {
     expect(calcularTotal(15000, 2, "")).toEqual(25500);
   });
+  it("deberia calcular para su respectivo descuento", () => {
+    expect(calcularTotal(15000, 2, "")).toEqual(25500);
+  });
+  it("deberia calcular para su respectivo descuento", () => {
+    expect(calcularTotal(5000, 2, "")).toEqual(9000);
+  });
+  it("deberia calcular para su respectivo descuento", () => {
+    expect(calcularTotal(3500, 2, "")).toEqual(6510);
+  });
+  it("deberia calcular para su respectivo descuento", () => {
+    expect(calcularTotal(1500, 2, "")).toEqual(2850);
+  });
+  it("deberia calcular para su respectivo descuento", () => {
+    expect(calcularTotal(500, 2, "")).toEqual(970);
+  });
+  it("deberia calcular para su respectivo descuento y estado", () => {
+    expect(calcularTotal(15000, 2, "UT")).toEqual(27495);
+  });
 });
 
 function impuestoEstado(estado) {
@@ -35,8 +53,23 @@ function impuestoEstado(estado) {
 
 function descuentoTotal(subTotal) {
   let descuento = 0;
-  if (subTotal == 30000){
+  if (subTotal == 30000)
     descuento = subTotal * 0.15
+  else{
+    if(subTotal == 10000)
+      descuento = subTotal * 0.10
+    else{
+      if(subTotal == 7000)
+        descuento = subTotal * 0.07
+      else{
+        if(subTotal == 3000)
+          descuento = subTotal * 0.05
+        else{
+          if(subTotal == 1000)
+            descuento = subTotal * 0.03
+        }
+      }
+    }
   }
   return descuento;
 }
